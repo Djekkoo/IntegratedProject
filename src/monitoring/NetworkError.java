70e9ce83be09d4469bdc22b51141058decd56e95
Joey <admin@joeyjo0.net>
2014-04-07 13:57:34 +0200

    Added monitoring and more routing.

--- src/monitoring/NetworkError.java
+++ src/monitoring/NetworkError.java
@@ -1,8 +1,10 @@
+package monitoring;
+
 /**
- * These classes are used for Routing in the Ad Hoc network.
- *
  * @author      Joey Haas <j.haas@student.utwente.nl>
  * @version     0.1
  * @since       2014-04-07
  */
-package routing;
+public enum NetworkError {
+	NOKEEPALIVE,DROPPED;
+}
--- src/monitoring/package-info.java
+++ src/monitoring/package-info.java
@@ -1,8 +1,6 @@
 /**
- * These classes are used for Routing in the Ad Hoc network.
- *
  * @author      Joey Haas <j.haas@student.utwente.nl>
  * @version     0.1
  * @since       2014-04-07
  */
-package routing;
+package monitoring;
