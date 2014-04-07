dd73213a8fea66abe163749b64b4145dc30baeb2
Djekkoo <djekkoo@hotmail.com>
2014-04-07 14:51:23 +0200

    Changed send-method

--- src/main/Communication.java
+++ src/main/Communication.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.LinkedList;
+
 import routing.RoutingInterface;
 import networking.DataPacket;
 import networking.Networker;
@@ -8,20 +10,23 @@ public class Communication {
 
 	Networker network;
 	RoutingInterface router;
+	LinkedList<DataPacket> routerQueue = new LinkedList<DataPacket>();
 	
 	public Communication() {
 		
 		network = new networking.Networker();
 		router = new routing.LinkStateRouting(new Callback(this, "routerPolling"));
 		
-		router.initialize(network);
+		router.initialize(new Callback(network, "send"));
 		
 	}
 	
 	public DataPacket routerPolling() {
 		
+		if (this.routerQueue.size() == 0)
+			return null;
 		
-		return null;
+		return this.routerQueue.removeFirst();
 		
 	}
 	
