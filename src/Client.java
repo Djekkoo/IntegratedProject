4d6b6757a84f70a098c90a3bdbaadaa29c176b8b
FlorianFFF <florianffff@gmail.com>
2014-04-07 13:49:20 +0200

    Structuur gefixed, en leipere background jewzlf

--- src/Client.java
+++ src/Client.java
@@ -6,8 +6,28 @@ public class Client {
 	
 	public Client(Communication communication) {
 		this.communication = communication;
-		
 		//Start de GUI
 		gui = new GUI(this);
+	}
+	
+	//Communicatie functies:	
+
+	public void sendChat(String text) {
+		//stuur naar communication
+	}
+
+	public void receiveChat(String msg) {
+		gui.updateChat(msg);
+	}
+
+	// Uitgaande van een string die de users splitst met ; zonder verdere spaties etc.
+	public void updateUsers(String users) {
+		String[] lijstje = new String[users.split(";").length];
+		for (int i = 0; i < users.split(";").length; i++) {
+			if (users.split(";")[i] != null) {
+				lijstje[i] = users.split(";")[i];
+			}
+		}
+		gui.updateUserlist(lijstje);
 	}
 }
